db36c95634e11eb9d2ef38e074f7556126a33f1b
abhinav <abhinavbhar2004@gmail.com>
2023-06-12 13:33:06 +0530

    my-array added

---
+++ Array.ts
@@ -0,0 +1,12 @@
+type user ={
+    name:string;
+    isActive:boolean;
+    isPaid:boolean;
+}
+const allUsers : user[]=[]
+allUsers.push({name:"abhinav", isActive:false,isPaid:true})
+const threeD: number[][]=[
+    [15,4,5,6],
+    [54,878,8,8]
+]
+// array of arrays and basic array input validation 

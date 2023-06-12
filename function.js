2392a9f48b62b27e6695148d501ff56d63c77cd5
abhinav <abhinavbhar2004@gmail.com>
2023-06-12 12:32:50 +0530

    functions in typescript

---
+++ function.js
@@ -0,0 +1,7 @@
+var gethello = function (s) {
+    return "";
+};
+var heroes = ["thor", "spiderman", "ironman"];
+heroes.map(function (hero) {
+    return "hero is ".concat(hero);
+});
---
+++ function.ts
@@ -0,0 +1,9 @@
+const gethello = (s:string):string=>{
+    return "";
+
+
+}
+const heroes = ["thor", "spiderman","ironman"]
+heroes.map(hero => {
+    return `my hero is ${hero}`
+})
